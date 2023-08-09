3aa2a601965da4bd8b41b4ed6f849f0e44fa8795
Abhigyan <abhigyanritiz63@gmail.com>
2023-08-09 13:24:47 +0530

    Till integrating ZOD

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,26 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+
+import "../globals.css";
+
+export const metadata = {
+  title: "Strings",
+  description: "A Next.js 13 Meta Strings Application",
+};
+
+const inter = Inter({ subsets: ["latin"] });
+
+type props = {
+  children: React.ReactNode;
+  propTwo: React.JSX.Element;
+};
+
+export default function RootLayout({ children, propTwo }: props) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
