f979a78ab2611aa7a8236defeb89e11ec31c3915
Abhigyan <abhigyanritiz63@gmail.com>
2023-09-04 11:02:51 +0530

    completed till activity

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 import Strings from "../models/strings.model";
+import { FilterQuery, SortOrder } from "mongoose";
 
 interface Params{
     userId:string,
@@ -91,4 +92,82 @@ export async function fetchUserPosts(userId: string) {
     } catch (error:any) {
         throw new Error(`Failed to fetch user strings: ${error.message}`);
     }
+}
+
+export async function fetchUsers({
+    userId,
+    searchString = "",
+    pageNumber = 1,
+    pageSize = 20,
+    sortBy="desc",
+}: {
+        userId: string;
+        searchString?: string;
+        pageNumber?: number;
+        pageSize?: number;
+        sortBy?: SortOrder;
+}) {
+    try {
+        connectToDB();
+
+        const skipAmount = (pageNumber - 1) * pageSize;
+
+        const regex = new RegExp(searchString, "i");
+
+        const query:FilterQuery<typeof User> = {
+            id:{$ne:userId}
+        }
+
+        if (searchString.trim() !== '') {
+            query.$or = [
+                { username: { $regex: regex } },
+                { name: { $regex: regex } },
+            ]
+        }
+
+        const sortOptions = { createdAt: sortBy };
+
+        const usersQuery = User.find(query).sort(sortOptions).skip(skipAmount).limit(pageSize);
+
+        const totalUserCount = await User.countDocuments(query);
+
+        const users = await usersQuery.exec();
+
+        const isNext = totalUserCount > skipAmount + users.length;
+
+        return {users, isNext}
+        
+    } catch (error:any) {
+        throw new Error(`Failed to fetch users: ${error.message}`);
+    }
+}
+
+export async function getActivity(userId: string) {
+    try {
+        connectToDB();
+
+        //find all threads created by the user
+        const userStrings = await Strings.find({ author: userId });
+        
+        //Collect all the child thread ids (replies) from the 'children' field
+        const childStringsIds = userStrings.reduce((acc, userStrings) => {
+            return acc.concat(userStrings.children)
+        },[])
+
+        //find all the strings that the user has replied to
+        const replies = await Strings.find({
+            _id: { $in: childStringsIds },
+            author:{$ne:userId}
+        }).populate({
+            path: 'author',
+            model: User,
+            select:'name image _id'
+        })
+
+        return replies;
+
+    } catch (error:any) {
+        throw new Error(`Failed to fetch activity: ${error.message}`);
+    }
+
 }
